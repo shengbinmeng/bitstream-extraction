24b2d2c233233893ec71f66c7d77f348c2eb450a
unknown <shengbinmeng@gmail.com>
2012-10-17 10:52:48 +0800

    code before moving to the server; many bugs exist: PNSR.m double type; e_pkt should be re-initialized every loop; first frame not include; and so on

--- Extract.bat
+++
@@ -1,2 +0,0 @@
-..\bin\BitStreamExtractorStatic ..\Foreman\str\Foreman.264 ..\Foreman\str\Discard_Group_t3q1.264 -et ..\Foreman\trc\Discard_Group_t3q1.txt 
-..\bin\H264AVCDecoderLibTestStatic ..\Foreman\str\Discard_Group_t3q1.264 ..\Foreman\yuv\Discard_Group_t3q1.yuv 
